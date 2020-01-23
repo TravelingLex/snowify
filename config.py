02179efa97b2542d77a4cd9c179b79f89fa67003
Alex Meyer <alex.meyer62@gmail.com.com>
2020-01-22 21:51:19 -0800

    Updated

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = 'cbe14ed2a206c188303087c3bfd80202'
-api_id = 'fd2cdee9'
-snow_url = 'https://api.weatherunlocked.com/api/snowreport/13003?hourly_interval=6&app_id=fd2cdee9&app_key=cbe14ed2a206c188303087c3bfd80202'
-forecast_url = 'https://api.weatherunlocked.com/api/resortforecast/13003?app_id=fd2cdee9&app_key=cbe14ed2a206c188303087c3bfd80202'
